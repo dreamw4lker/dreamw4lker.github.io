57e6a3f2b1f88a5724c3fb1cf3ffc9f841e85d41
Alexander Shkirkov <can3456@mail.ru>
2023-06-10 14:24:06 +0700

    setup project build, add docs folder

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -38,4 +38,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    outDir: 'docs'
+  }
 })
