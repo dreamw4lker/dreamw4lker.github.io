5b86e13b16f6540ecbb19728a0d029d24b551515
Alexander Shkirkov <can3456@mail.ru>
2023-08-17 15:41:39 +0700

    Added some commercial projects info

---
+++ src/components/ProjectTile.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-col sm="12" md="6">
+    <v-card
+      class="mx-auto x-card"
+    >
+      <v-card-title class="bg-primary-transparent text-white" style="border-bottom: 1px solid rgba(255, 255, 255, 0.2)">
+        {{ project.name }}
+      </v-card-title>
+      <v-card-text class="text-white pa-4">
+        <p class="text-pre-wrap mb-6" style="font-size: 16px">{{ project.description }}</p>
+        <template v-for="link in project.links">
+          <p class="font-weight-bold mb-2">{{ `${link.type}: ` }}<a :href="link.href"
+                                                           style="color: #66bb6a">{{ link.href }}</a></p>
+        </template>
+
+        <p class="font-weight-bold">Main libraries / technologies:</p>
+        <ul class="ml-4">
+          <template v-for="tech in project.technologies">
+            <li>{{ tech }}</li>
+          </template>
+        </ul>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</template>
+
+<script setup>
+defineProps({
+  project: Object,
+})
+</script>
+
+<style scoped>
+.bg-primary-transparent {
+  background-color: rgba(48, 43, 99, 0.7) !important;
+}
+
+.x-card {
+  backdrop-filter: blur(3px);
+  background-color: rgba(255, 255, 255, 0.1) !important;
+  border: 1px solid rgba(255, 255, 255, 0.2) !important;
+  border-radius: 10px !important;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25) !important;
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,60 +1,85 @@
 <template>
   <div class="d-flex flex-column mx-auto" style="max-width: 1200px">
     <p class="text-white text-h3 text-center mt-4">Projects</p>
+    <p class="text-grey-lighten-2 text-h6 text-center mt-4">Open source</p>
 
     <v-row class="pt-4">
-      <template v-for="project in projects">
-        <v-col sm="12" md="6">
-          <v-card
-            class="mx-auto x-card"
-          >
-            <v-card-title class="bg-primary-transparent text-white" style="border-bottom: 1px solid rgba(255, 255, 255, 0.2); font-size: ">
-              {{ project.name }}
-            </v-card-title>
-            <v-card-text class="text-white pa-4">
-              <p class="text-pre-wrap mb-6" style="font-size: 16px">{{ project.description }}</p>
-              <p class="font-weight-bold mb-2">Github link: <a :href="project.link" style="color: #66bb6a">{{ project.link}}</a></p>
-              <p class="font-weight-bold">Main libraries / technologies:</p>
-              <ul class="ml-4">
-                <template v-for="tech in project.technologies">
-                  <li>{{ tech }}</li>
-                </template>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
+      <template v-for="project in projectsOpenSource">
+        <project-tile :project="project"/>
+      </template>
+    </v-row>
+
+    <p class="text-grey-lighten-2 text-h6 text-center mt-4">Commercial, publicly available</p>
+    <v-row class="pt-4">
+      <template v-for="project in projectsCommercial">
+        <project-tile :project="project"/>
       </template>
     </v-row>
   </div>
 </template>
 
 <script setup>
-const projects = [
+import ProjectTile from "@/components/ProjectTile"
+
+const projectsOpenSource = [
   {
     name: 'gitea-webhook-notifier',
-    link: 'https://github.com/dreamw4lker/gitea-webhook-notifier',
+    links: [
+      {
+        type: 'Github link',
+        href: 'https://github.com/dreamw4lker/gitea-webhook-notifier'
+      }
+    ],
     description: 'Notify about push events in any Gitea repositories.\nBased on Gitea webhook events.',
     technologies: ['Java 11', 'Maven', 'Thymeleaf']
   },
   {
     name: 'i18n-to-xls',
-    link: 'https://github.com/dreamw4lker/i18n-to-xls',
+    links: [
+      {
+        type: 'Github link',
+        href: 'https://github.com/dreamw4lker/i18n-to-xls',
+      }
+    ],
     description: 'Convert JSON i18n strings to XLS-file and vice versa.\nCan be used with vue-i18n or similar libraries.',
     technologies: ['Java 11', 'Maven', 'GSON', 'Apache POI'],
   }
 ]
+
+const projectsCommercial = [
+  {
+    name: 'Ovum Lab',
+    links: [
+      {
+        type: 'PlayMarket',
+        href: 'https://play.google.com/store/apps/details?id=ru.lynxsoft.ovum.mobile',
+      },
+      {
+        type: 'AppStore',
+        href: 'https://apps.apple.com/app/laboratoriya-ovum/id1504379583'
+      }
+    ],
+    description: 'A mobile application that allows to make an appointment to a doctor and see the history of your previous visits',
+    technologies: ['Apache Cordova', 'Vue 2', 'Vuetify.js']
+  },
+  {
+    name: 'Janti',
+    links: [
+      {
+        type: 'PlayMarket',
+        href: 'https://play.google.com/store/apps/details?id=ru.janti.mobile',
+      },
+      {
+        type: 'AppStore',
+        href: 'https://apps.apple.com/ru/app/%D0%BC%D0%BF-janti/id1495735394'
+      }
+    ],
+    description: 'A mobile application that allows to monitor road transport, build a passed routes and some technical reports',
+    technologies: ['Apache Cordova', 'Vue 2', 'JavaScript', 'OpenLayers']
+  },
+]
 </script>
 
 <style scoped>
-.bg-primary-transparent {
-  background-color: rgba(48, 43, 99, 0.7) !important;
-}
 
-.x-card {
-  backdrop-filter: blur(3px);
-  background-color: rgba(255, 255, 255, 0.1) !important;
-  border: 1px solid rgba(255, 255, 255, 0.2) !important;
-  border-radius: 10px !important;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25) !important;
-}
 </style>
