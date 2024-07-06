aba6f1dec090e7a643237c426bb276548cbf9357
Alexander Shkirkov <10252565+dreamw4lker@users.noreply.github.com>
2024-07-06 12:22:11 +0700

    Cleanup docs folder due to automatic build

--- docs/assets/ssrBoot-0be5f275.js
+++
@@ -1,1 +0,0 @@
-import{s,X as t,h as e,Z as n}from"./index-c397a54d.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
