e2e7d18b0e01a78fd58678443d59c428b31247fa
Alexander Shkirkov <10252565+dreamw4lker@users.noreply.github.com>
2024-07-13 16:22:29 +0700

    Moved particles setup scripts to separate plugin file

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,16 +1,12 @@
 import {getRouter} from "./router/index.js"
 import vuetify from "./vuetify/vuetify.js"
 import Particles from "@tsparticles/vue3"
-import {loadSlim} from "@tsparticles/slim"
+import {initParticles} from "@/plugins/particles/particles.js"
 
 export function registerPlugins(app) {
   let router = getRouter(app)
   app
     .use(vuetify)
     .use(router)
-    .use(Particles, {
-      init: async engine => {
-        await loadSlim(engine)
-      }
-    })
+    .use(Particles, initParticles())
 }
